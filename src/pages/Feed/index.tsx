2fb16951f9ad28b041d714732d308312490e5293
johelder <johelder.arj@gmail.com>
2021-08-08 15:34:16 -0300

    create requisition on Feed

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {useEffect} from 'react';
+import {useState} from 'react';
+import {FlatList, Text} from 'react-native';
+import Card from '../../components/Card';
+import api from '../../services/api';
 
-const Feed: React.FC = () => (
-  <View>
-    <Text>Feed</Text>
-  </View>
-);
+import * as S from './styles';
+
+interface CharacterProps {
+  id: string;
+  name: string;
+  status: string;
+  species: string;
+  gender: string;
+  location: {
+    name: string;
+  };
+  image: string;
+}
+
+const Feed: React.FC = () => {
+  const [feed, setFeed] = useState<CharacterProps[]>([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const loadFeed = async (currentPage = page) => {
+      const response = await api.get(`character/?page=${currentPage}`);
+
+      setFeed(response.data.results);
+    };
+
+    loadFeed();
+  }, [page]);
+
+  console.log(feed);
+
+  return (
+    <S.Container>
+      <S.Content>
+        <FlatList
+          data={feed}
+          keyExtractor={item => item.id}
+          renderItem={({item: character}) => <Card character={character} />}
+        />
+      </S.Content>
+    </S.Container>
+  );
+};
 
 export default Feed;
---
+++ src/pages/Feed/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled.View`
+  width: 90%;
+  margin: 0 auto;
+`;
