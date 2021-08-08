09849dadc90f21e1e9232582edc8c8063dad88a8
johelder <johelder.arj@gmail.com>
2021-08-08 20:39:29 -0300

    check scrolling list end

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import {useCallback} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {FlatList} from 'react-native';
-import Card from '../../components/Card';
+
 import api from '../../services/api';
+
+import Card from '../../components/Card';
 
 import * as S from './styles';
 
@@ -24,13 +25,19 @@ interface CharacterProps {
 const Feed: React.FC = () => {
   const [feed, setFeed] = useState<CharacterProps[]>([]);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const loadFeed = useCallback(async () => {
+    if (page === totalPages) {
+      return;
+    }
+
     const response = await api.get(`character/?page=${page}`);
 
+    setTotalPages(response.data.info.pages);
     setFeed([...feed, ...response.data.results]);
     setPage(page + 1);
-  }, [feed, page]);
+  }, [feed, page, totalPages]);
 
   useEffect(() => {
     loadFeed();
