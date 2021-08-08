ad04e51da0f71923259924ba94bccccede1af620
johelder <johelder.arj@gmail.com>
2021-08-08 20:37:47 -0300

    add style on card

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -2,13 +2,17 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
+  height: 150px;
 
   flex-direction: row;
+  background: #f0f0f5;
+
+  margin: 15px 0;
+  border-radius: 6px;
 `;
 
 export const CharacterImage = styled.Image`
   width: 40%;
-  height: 150px;
 `;
 export const Content = styled.View``;
 export const Name = styled.View``;
