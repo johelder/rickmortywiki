90706f20cb12c04e1e138ce5f845d76c5d5fd208
johelder <johelder.arj@gmail.com>
2021-08-20 22:17:52 -0300

    create error component

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState, useCallback} from 'react';
 
 import api from '../../services/api';
-import * as S from './styles';
+
 import List from '../../components/List';
 import Input from '../../components/Input';
-
 // import Err from '../../components/Err';
+
+import * as S from './styles';
+import Err from '../../components/Err';
 
 export interface CharacterProps {
   id: string;
@@ -26,24 +28,31 @@ const Feed: React.FC = () => {
   const [feed, setFeed] = useState<CharacterProps[]>([]);
   const [page, setPage] = useState(2);
   const [loading, setLoading] = useState(false);
-  const [totalPages, setTotalPages] = useState(null);
+  const [totalPages, setTotalPages] = useState(0);
   const [totalCharacters, setTotalCharacters] = useState(null);
 
   const [searchText, setSearchText] = useState('');
-  const [filteredFeed, setFilteredFeed] = useState<CharacterProps[] | null>(
-    null,
-  );
+  const [filteredFeed, setFilteredFeed] = useState<CharacterProps[] | any>();
+  const [filteredPage, setFilteredPage] = useState(2);
+  const [totalFilteredPage, setTotalFilteredPage] = useState(0);
+
+  const [err, setErr] = useState('');
 
   useEffect(() => {
     const getCharacters = async () => {
       setLoading(true);
-      const response = await api.get('character/');
+      try {
+        const response = await api.get('character/');
 
-      const data = [...response.data.results];
-      setFeed(data);
-      setTotalPages(response.data.info.pages);
-      setTotalCharacters(response.data.info.count);
-      setLoading(false);
+        const data = [...response.data.results];
+        setFeed(data);
+        setTotalPages(response.data.info.pages);
+        setTotalCharacters(response.data.info.count);
+        setLoading(false);
+        setErr('');
+      } catch (error) {
+        setErr('Não foi possível carregar os personagens.');
+      }
     };
     getCharacters();
   }, []);
@@ -59,23 +68,48 @@ const Feed: React.FC = () => {
       const data = [...response.data.results];
       setFeed([...feed, ...data]);
       setPage(page + 1);
+      setErr('');
     } catch (error) {
-      console.log(error);
+      setErr('Não foi possível carregar mais personagens');
     }
   }, [page, feed, totalPages]);
 
   const filterFeed = useCallback(async () => {
-    if (searchText === '') {
-      setPage(1);
-    }
-    setLoading(true);
-    const response = await api.get(`character/?name=${searchText}`);
+    try {
+      if (searchText === '') {
+        setPage(1);
+      }
+      setLoading(true);
+      const response = await api.get(`character/?name=${searchText}`);
 
-    const data = [...response.data.results];
-    setFilteredFeed(data);
-    setTotalCharacters(response.data.info.count);
-    setLoading(false);
+      const data = [...response.data.results];
+      setFilteredFeed(data);
+      setTotalCharacters(response.data.info.count);
+      setTotalFilteredPage(response.data.info.pages);
+      setLoading(false);
+      setErr('');
+    } catch (error) {
+      setErr('Personagem não encontrado.');
+    }
   }, [searchText]);
+
+  const updateFilteredFeed = useCallback(async () => {
+    try {
+      if (filteredPage > totalFilteredPage) {
+        return;
+      }
+
+      const response = await api.get(
+        `character/?page=${filteredPage}&name=${searchText}`,
+      );
+
+      setFilteredFeed([...filteredFeed, ...response.data.results]);
+      setFilteredPage(filteredPage + 1);
+      setErr('');
+    } catch (error) {
+      setErr('Não foi possível carregar mais personagens');
+    }
+  }, [filteredFeed, filteredPage, searchText, totalFilteredPage]);
 
   return (
     <S.Container>
@@ -89,20 +123,19 @@ const Feed: React.FC = () => {
           value={searchText}
           onChangeText={text => {
             setSearchText(text);
-            filterFeed();
           }}
           onSubmitEditing={filterFeed}
         />
 
-        {loading ? (
+        {err ? (
+          <Err message={err} />
+        ) : loading ? (
           <S.LoadingFeed size="small" color="#1e2047" />
         ) : filteredFeed ? (
-          <List feed={filteredFeed} />
+          <List feed={filteredFeed} loadFeed={updateFilteredFeed} />
         ) : (
           <List feed={feed} loadFeed={updateFeed} />
         )}
-
-        {}
       </S.Content>
     </S.Container>
   );
